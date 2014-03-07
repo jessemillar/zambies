3da5873dfb68739b11b7e9ed2f5593aa03ea632a
Jesse Millar <jesse@jessemillar.com>
2014-03-07 13:19:14 -0700

    Organized project folders and made the tilt-to-pick-the-direction-to-shoot-in controls more intuitive.

--- main/Source/AppDelegate.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "EJAppViewController.h"
-
-@interface AppDelegate : NSObject <UIApplicationDelegate>
-
-@property (nonatomic, retain) IBOutlet UIWindow *window;
-@end
-
--- main/Source/Ejecta/EJCanvas/EJDrawable.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "EJTexture.h"
-
-@protocol EJDrawable
-
-@property (readonly, nonatomic) EJTexture *texture;
-
-@end
